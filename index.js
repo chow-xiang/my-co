2676eeef0ff60e066b4ba1e8b86404c6d4dad2ec
周祥 <zx22962@ly.com>
2017-04-04 12:49:23 +0800

    op

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var co = require('./co');
+// var co = require('./co');
+
+var co = require('./my-co');
 var http = require('http');
 
 function* test(){
@@ -49,64 +51,5 @@ co(test).then(val => {console.log(val)})
 // testC();
 
 
-
-
-
-
-
-// function co(gen) {
-	
-// 	return new Promise(function (resolve, reject) {
-		
-// 		var interator = gen();
-// 		var result    = toNext(interator);
-
-// 		var toFulled = toFulledFactory(resolve, reject)
-// 		toFulled(interator, result)
-// 	})
-// }
-
-function toFulledFactory(resolve, reject){
-
-	return function toFulled(interator, result){
-
-		if(result.done){return resolve(result.value)}
-
-		if(result && typeof result.then == 'function'){
-
-			result.then((ret) => {
-				result = toNext(interator, ret);
-				toFulled(interator, result)
-			})
-		}else{
-
-			result = toNext(interator, result.value);
-			toFulled(interator, result)
-		}
-	}
-}
-
-
-
-
-function toNext (interator, value) {
-	
-	var result = interator.next(value);
-	if(typeof result.value == 'function'){
-		return funToPromise(result.value)
-	}
-	return result;
-}
-
-
-
-function funToPromise (fun) {
-	return new Promise((resolve, reject) => {
-		fun((error, result) => {
-			if(error) reject(error);
-			if(result) resolve(result);
-		});
-	})
-}
 
 
